8f966982a0654c6b7bf3aad3aac105e6a21760d1
hothaifa zoubi <45634350+hothaifa96@users.noreply.github.com>
2019-01-09 20:16:24 +0200

    Add files via upload

---
+++ Exam1/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1
+{
+	class Program
+	{
+		
+		static void Main(string[] args)
+		{
+			//q1(); 
+			//q2();
+			//q3();
+			//q4();
+			//q5(5,5,189,54);
+			//q6();in a text file 
+			firstQITheSecondePart();
+		}
+
+		private static void firstQITheSecondePart()
+		{
+			Random rnd = new Random();
+			int[] a = new int[rnd.Next(15)];
+			int[] b = new int[rnd.Next(15)];
+			for (int i = 0; i < a.Length; i++)
+			{
+				a[i] = rnd.Next(-200, 200);
+			}
+			for (int i = 0; i < b.Length; i++)
+			{
+				b[i] = rnd.Next(-200, 200);
+			}
+			Console.WriteLine(	 Measure( a, b));
+			
+		}
+		private static int Measure( int[] a, int[] b)
+		{
+			int acounter = 0, bcounter = 0;
+			int len = a.Length;
+			if (b.Length < a.Length)
+				len = b.Length;
+			for (int i = 0; i < len; i++)
+			{
+				if (a[i] > b[i])
+					acounter++;
+				else
+					if (a[i] < b[i])
+					bcounter++;
+				}
+			if (acounter > bcounter)
+					return 1;
+			else
+				if (acounter < bcounter)
+					return -1;
+			else return 0;
+			
+		}
+
+
+
+
+		private static void q5(params int [] show)
+		{
+			for (int i = 0; i < show.Length; i++)
+			{
+				Console.Write(show[i] + " ");
+			}
+			Console.WriteLine();
+		}//params
+
+		private static void q4()
+		{
+			int[] a, b;
+			InitializeArrays(out a,out b);
+			for (int i = 0; i < a.Length; i++)
+			{
+				Console.WriteLine(a[i]);
+			}
+			for (int i = 0; i < b.Length; i++)
+			{
+				Console.WriteLine(b[i]);
+			}
+		}
+		private static void InitializeArrays(out int[] a,out int[] b)
+		{
+			Random r = new Random();
+			a = new int[10];
+			b = new int[10];
+			for (int i = 0; i < 10; i++)
+			{
+				a[i] = r.Next(101);
+				b[i] = r.Next(101);
+			}
+		}//for q4
+
+			private static void q3()
+		{
+			int[] a = { 5, 4, -5 };
+			Updateq3(a);
+			Console.WriteLine(a[0]);
+		}
+		static void Updateq3(int[] a)
+		{
+			a = new int[3];
+			a[0] = 1;
+		}//for q3
+
+
+
+		private static void q2()
+		{
+			int[] a = { 5, 4, -5 };
+			Updateq3(a);
+			
+		}
+		static void Updateq2(int[] a)
+		{
+			a[0] = 1;
+		}//for q2
+		  
+
+
+		static void q1()
+		{
+			Console.WriteLine("Enter a number:");
+			int x = Convert.ToInt32(Console.ReadLine());
+			Power2(ref x);
+			Console.WriteLine($"power 2 of your number is {x}");
+		}
+		private static void Power2(ref int i)
+		{
+			i = i * 2;
+		}// for q1
+
+	
+	}
+}
---
+++ winform/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform
+{
+	public partial class Form1 : Form
+	{
+		Random r = new Random();
+		int score = 0; int full = 0;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			memorygamesfirstposition();
+		}
+		private void memorygamesfirstposition()
+		{
+			label1.Text = $"{r.Next(500)}";
+			label2.Text = $"{r.Next(500)}";
+			label3.Text = $"{r.Next(500)}";
+			label4.Text = $"{r.Next(500)}";
+			label5.Text = $"{r.Next(500)}";
+			label1.Visible = true;
+			label2.Visible = true;
+			label3.Visible = true;
+			label4.Visible = true;
+			label5.Visible = true;
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+			textBox4.Text = "";
+			textBox5.Text = "";
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Are you sure you want to Exit?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+				   == DialogResult.Yes)
+			{
+				Application.Exit();
+			}
+		}
+
+		private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("this game made in a Exam and its not perfect as it should be ");
+		}
+
+		private void startBt_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+			textBox4.Text = "";
+			textBox5.Text = "";
+			label1.Visible = false;
+			label2.Visible = false;
+			label3.Visible = false;
+			label4.Visible = false;
+			label5.Visible = false;
+		}
+
+		private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			score = 0;
+			memorygamesfirstposition();
+		}
+
+		private void textBox1_Leave(object sender, EventArgs e)
+		{
+			if (textBox1.Text != "")
+				full++;
+			if (textBox1.Text == label1.Text)
+			{
+				
+				score++;
+				textBox1.ForeColor = Color.Green;
+			}
+			else textBox1.ForeColor = Color.Red;
+			if (full == 5)
+			{
+				callit();
+
+			}
+		}
+
+		private void textBox2_Leave(object sender, EventArgs e)
+		{
+			if (textBox2.Text != "")
+				full++;
+			if (textBox2.Text == label2.Text )
+			{
+				score++;
+				textBox2.ForeColor = Color.Green;
+			}
+			else textBox2.ForeColor = Color.Red;
+			if (full == 5)
+			{
+				callit();
+
+			}
+		}
+
+		private void textBox3_Leave(object sender, EventArgs e)
+		{
+			if (textBox3.Text != "")
+				full++;
+			if (textBox3.Text == label3.Text)
+			{
+				score++;
+				textBox3.ForeColor = Color.Green;
+			}
+			else textBox3.ForeColor = Color.Red;
+			if (full == 5)
+			{
+				callit();
+
+			}
+		}
+
+		private void textBox4_Leave(object sender, EventArgs e)
+		{
+			if (textBox4.Text != "")
+				full++;
+			if (textBox4.Text == label4.Text)
+			{
+				score++;
+				textBox4.ForeColor = Color.Green;
+			}
+			else textBox4.ForeColor = Color.Red;
+			if (full == 5)
+			{
+				callit();
+
+			}
+		}
+
+		private void textBox5_Leave(object sender, EventArgs e)
+		{
+			if (textBox5.Text != "")
+				full++;
+			if (textBox5.Text == label5.Text)
+			{
+				score++;
+				textBox5.ForeColor = Color.Green;
+			}
+			else textBox5.ForeColor = Color.Red;
+			if (full == 5)
+			{
+				callit();
+
+			}
+		}
+
+		private void callit()
+		{
+			if (score == 5)
+				MessageBox.Show("Yuoooofii you have them all right");
+			else
+				MessageBox.Show("oops !!you missed some");
+			listBox1.Items.Add($"score :{score}");
+		}
+	}
+}
